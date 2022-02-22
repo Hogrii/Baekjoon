73dbf67228d372f489818a20f0bbc6032df4b018
Hogrii <hosboy93@naver.com>
2022-02-22 18:37:01 +0900

    stage2 clear

---
+++ Stage2/src/Ex5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int hour;
+		int minute;
+		hour = sc.nextInt();
+		minute = sc.nextInt();
+		minute = minute - 45;
+		if(minute<0) {
+			hour = hour - 1;
+			minute = minute + 60;
+			if(hour<0) {
+				hour = hour + 24;
+			}
+		}
+		System.out.println(hour + " " + minute);
+	}
+}
