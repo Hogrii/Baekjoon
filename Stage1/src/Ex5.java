4cf88242879cdb3dc80536211e2c6f0d565a88be
Hogrii <hosboy93@naver.com>
2022-02-23 20:09:29 +0900

    stage1 clear

--- Stage1/src/Ex5.java
+++ Stage1/src/Ex5.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Ex5 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);		
 		int A;
 		int B;
 		A = sc.nextInt();
-		B = sc.nextInt();
-		
+		B = sc.nextInt();		
 		System.out.println(A+B);
 	}
 }
