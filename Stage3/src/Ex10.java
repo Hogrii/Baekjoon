f902e1bd562dfaabb3572a68094a6aaaeddb9a7e
Hogrii <hosboy93@naver.com>
2022-02-23 20:07:04 +0900

    stage3 clear

--- Stage3/src/Ex10.java
+++ Stage3/src/Ex10.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class Ex10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		
+		int N = sc.nextInt();
+		for(int i=0; i<N; i++) {
+			
+			for(int j=N-1; j>i; j--) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
