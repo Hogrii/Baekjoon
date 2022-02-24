8807760f8e5c1a4dcfa59414de63d64773b50a14
Hogrii <hosboy93@naver.com>
2022-02-24 18:28:38 +0900

    stage5 clear

---
+++ stage5/src/Ex6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = Integer.parseInt(sc.nextLine());
+		int sum = 0;
+		int count = 0;
+		for(int i=0; i<T; i++) {
+			String str = sc.nextLine();
+			for(int j=0; j<str.length(); j++) {
+				if(str.charAt(j)=='O') {
+					count++;
+					sum = sum + count;
+				}else {
+					count = 0;
+				}
+			}
+			System.out.println(sum);
+			sum = 0;
+			count = 0;
+		}
+	}
+}
