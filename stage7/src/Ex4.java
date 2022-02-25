437d086dec231d6b4731afdd20b9f56da15f3293
Hogrii <hosboy93@naver.com>
2022-02-25 22:18:11 +0900

    stage7 continue

--- stage7/src/Ex4.java
+++ stage7/src/Ex4.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Ex4 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int i=0; i<T; i++) {
+			int R = sc.nextInt();
+			String str = sc.next();
+			String[] strRepeat = new String[str.length()*R];
+			for(int j=0; j<str.length(); j++) {
+				String substr = str.substring(j, j+1);
+				for(int k=0; k<R; k++) {
+					strRepeat[k] = substr;
+					System.out.print(strRepeat[k]);
+				}
+			}
+			System.out.println();
+		}
+	}
 }
---
+++ stage7/src/Ex6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		// 공백제거
+		// trim() vs strip() -> trim보다 strip이 더 광범위하게 공백을 제거
+		str = str.strip();
+		// str의 단어 사이에 공백이 여러개 있을 경우 1개의 공백을 제외하고 제거
+		str = str.replaceAll("\\s+", " ");
+		String[] array = str.split(" ");
+		int count = 0;
+		for(int i=0; i<array.length; i++) {
+			count++;			
+		}		
+		// str에 공백 하나만 입력되었을 경우
+		if(array.length==1 && array[0]=="") {
+			count = 0;
+		}
+		System.out.println(count);
+	}
+}
