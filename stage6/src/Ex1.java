74d0631d6a01bbd95e6f39f14ed12a80f7ccfbf4
Hogrii <hosboy93@naver.com>
2022-02-28 21:02:12 +0900

    stage6 clear

---
+++ stage6/src/Ex1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex1 {
+	public static long sum(int[] a) {
+		long sum = 0;
+		for(int i=0; i<a.length; i++) {
+			sum = sum + a[i];
+		}		
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] a =  new int[n];
+		
+		for(int i=0; i<a.length; i++) {
+			a[i] = i+1;
+		}
+		
+		long sum = sum(a);
+		System.out.println(sum);
+	}
+}
