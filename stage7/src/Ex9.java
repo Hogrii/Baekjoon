e5d95b5fadccc0e4418feaa4bdd0be05f384aad6
Hogrii <hosboy93@naver.com>
2022-02-28 20:44:22 +0900

    stage7 clear

--- stage7/src/Ex9.java
+++ stage7/src/Ex9.java
@@ -4,17 +4,20 @@ public class Ex9 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		int count = 0;
-		for(int i=0; i<str.length(); i++) {
+		int count = 1;
+		for(int i=1; i<str.length(); i++) {
+			
 			if(str.charAt(i)=='=') {				
 				if(str.charAt(i-1)=='c' || str.charAt(i-1)=='s') {
 					
-				}else if(str.charAt(i-1)=='z') {					
-					if(str.charAt(i-2)=='d') {
-						count--;
-					}else {
+				}else if(str.charAt(i-1)=='z') {
+					if(i!=1) {
+						if(str.charAt(i-2)=='d') {
+							count--;
+						}else {
 						
-					}					
+						}
+					}
 				}				
 			}else if(str.charAt(i)=='-') {				
 				if(str.charAt(i-1)=='c' || str.charAt(i-1)=='d') {
