8bceb1fd3d18a16ec18a8709b64653d5e44a7132
Hogrii <hosboy93@naver.com>
2022-03-07 18:33:16 +0900

    stage9 continue

---
+++ stage9/src/Ex6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ex6 {
+	public static boolean[] prime = new boolean[10001];
+	public static void get_prime() {
+		prime[0] = prime[1] = true;
+		for(int i=2; i<=Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j=i*i; j<prime.length; j+=i) {
+				prime[j] = true;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		get_prime();
+		int T = sc.nextInt();
+		for(int i=0; i<T; i++) {
+			int n = sc.nextInt();
+			int first = n/2;
+			int second = n/2;
+			
+			while(true) {
+				if(!prime[first] && !prime[second]) {
+					System.out.println(first + " " + second);
+					break;
+				}else {
+					first--;
+					second++;
+				}
+			}
+		}
+	}
+}
