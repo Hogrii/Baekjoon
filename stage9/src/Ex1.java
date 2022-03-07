7772fad5cb68f27ca590456a046a9cfca8fac712
Hogrii <hosboy93@naver.com>
2022-03-07 14:23:21 +0900

    stage9 continue

--- stage9/src/Ex1.java
+++ stage9/src/Ex1.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
 
 public class Ex1 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] primeNum = new int[N];
+		int count = 0;
+		int moduleCount = 0;
 		
+		for(int i=0; i<N; i++) {
+			primeNum[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=1; j<primeNum[i]+1; j++) {
+				if(primeNum[i]==1) {
+					continue;
+				}else {
+					if(primeNum[i]%j == 0) {
+						moduleCount++;
+					}
+				}
+			}
+			if(moduleCount==2) {
+				count++;
+				moduleCount = 0;
+			}else {
+				moduleCount = 0;
+			}
+		}
+		System.out.println(count);
 	}
 }
