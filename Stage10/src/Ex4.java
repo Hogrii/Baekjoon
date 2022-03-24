609243ca9eb0e53f02d1172a1119b2bc5f2f2ca1
Hogrii <hosboy93@naver.com>
2022-03-24 15:55:08 +0900

    stage10 clear

--- Stage10/src/Ex4.java
+++ Stage10/src/Ex4.java
@@ -3,43 +3,36 @@ import java.util.Scanner;
 
 public class Ex4 {
  
-	public static StringBuilder sb = new StringBuilder();
- 
+	public static StringBuilder sb = new StringBuilder(); 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
- 
+		
+		// 원반의 수
 		int N = in.nextInt();
- 
+
+		// 원반 옮기기 경우의 수
 		sb.append((int) (Math.pow(2, N) - 1)).append('\n');
  
-		Hanoi(N, 1, 2, 3);
+		// 원반 옮기기
+		HanoiMove(N, 1, 2, 3);
 		System.out.println(sb);
  
 	}
  
-	/*
-		N : 원판의 개수 
-		start : 출발지 
-		mid : 옮기기 위해 이동해야 장소 
-		to : 목적지
-	 */
- 
-	public static void Hanoi(int N, int start, int mid, int to) {
-		// 이동할 원반의 수가 1개라면?
+	public static void HanoiMove(int N, int start, int mid, int to) {
+		// 원반이 1개 남았을 때
 		if (N == 1) {
 			sb.append(start + " " + to + "\n");
 			return;
-		}
- 
-		// A -> C로 옮긴다고 가정할 떄,
-		// STEP 1 : N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
-		Hanoi(N - 1, start, to, mid);
+		} 
+
+		// N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
+		HanoiMove(N - 1, start, to, mid);
     
-		// STEP 2 : 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 to지점으로 옮긴다.)
+		// 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 to지점으로 옮긴다.)
 		sb.append(start + " " + to + "\n");
     
-		// STEP 3 : N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 to 지점으로 옮긴다.)
-		Hanoi(N - 1, mid, start, to);
- 
+		// N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 to 지점으로 옮긴다.)
+		HanoiMove(N - 1, mid, start, to); 
 	}
 }
