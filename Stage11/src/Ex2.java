1ae497ea53749879adbb9bdebe4db8f2dda0354c
Hogrii <hosboy93@naver.com>
2022-03-25 15:07:06 +0900

    stage11 continue

--- Stage11/src/Ex2.java
+++ Stage11/src/Ex2.java
@@ -11,16 +11,20 @@ public class Ex2 {
 	
 	public static int cons(int N) {
 		for(int i=1; i<N; i++) {
+			// 생성자를 확인하기 위한 변수
 			int sum = i;
+			// while문에 제약을 걸기 위한 변수
 			int j= i;
 			while(j != 0) {
 				sum = sum + (j%10);
 				j = j/10;
 			}
+			// sum이 N과 같다면 생성자 i 리턴
 			if(sum==N) {
 				return i;
 			}
 		}
+		// 생성자가 없다면 0 리턴
 		return 0;
 	}
 }
--- Stage11/src/Ex3.java
+++ Stage11/src/Ex3.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] body = new int[N][2];
+		int[] ranking = new int[N];
 		
+		for(int i=0; i<N; i++) {
+			// ¸ö¹«°Ô
+			body[i][0] = sc.nextInt();
+			// Å°
+			body[i][1] = sc.nextInt();
+		}
+		
+		for(int i=0; i<N; i++) {
+			int rank = 1;
+			for(int j=0; j<N; j++) {
+				if(i==j) continue;
+				
+				if(body[i][0]<body[j][0] && body[i][1]<body[j][1]) {
+					rank++;
+				}
+			}
+			ranking[i] = rank;
+		}
+		
+		for(int i=0; i<N; i++) {
+			System.out.println(ranking[i]);
+		}		
 	}
 }
