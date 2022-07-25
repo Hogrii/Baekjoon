66f2c7bdc806ce11461b8314057b9e334ddbaaad
jinho <hosboy93@naver.com>
2022-07-25 17:52:02 +0900

    BOJ 10989 clear

--- Stage12/src/Ex03.java
+++ Stage12/src/Ex03.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Ex03 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
+		int arr[] = new int[Integer.parseInt(br.readLine())];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		br.close();
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<arr.length; i++) {
+			sb.append(arr[i]).append('\n');
+		}
+		
+		System.out.println(sb);
 	}
 }
