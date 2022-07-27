41d51ec062683b72df60e9731b61adb618eaf32e
jinho <hosboy93@naver.com>
2022-07-27 21:57:35 +0900

    BOJ 10814 주석추가

--- Stage12/src/Ex09.java
+++ Stage12/src/Ex09.java
@@ -10,21 +10,29 @@ public class Ex09 {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
+		// 회원의 수
 		int N = Integer.parseInt(br.readLine());
+		// 회원 배열
 		String arr[][] = new String[N][2];
 		
+		// 회원 정보 -> 나이, 이름
 		for(int i=0; i<N; i++) {
+			// " "을 기준으로 0에 나이, 1에 이름 입력
 			st = new StringTokenizer(br.readLine(), " ");
 			arr[i][0] = st.nextToken();
 			arr[i][1] = st.nextToken();
 		}
 		
+		// 정렬
 		Arrays.sort(arr, (s1, s2) -> {
+			// 나이순으로 정렬
 			return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
 		});
 		
+		// 출력
 		for(int i=0; i<N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
+			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
