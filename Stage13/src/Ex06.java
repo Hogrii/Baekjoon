d91e4b0cf3cbf2e0d1349f97c30e9e38d081ea92
jinho <hosboy93@naver.com>
2022-08-01 18:43:00 +0900

    BOJ 1269 대칭 차집합 완료

--- Stage13/src/Ex06.java
+++ Stage13/src/Ex06.java
@@ -1,6 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Ex06 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		Set<Integer> setA = new HashSet<>(); // 1 2 4
+		Set<Integer> setB = new HashSet<>(); // 2 3 4 5 6
 		
+		st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<A; i++) {
+			setA.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<B; i++) {
+			setB.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int SDS = 0;
+		for(int key : setA) {
+			if(!setB.contains(key)) {
+				SDS++;
+			}
+		}
+		for(int key : setB) {
+			if(!setA.contains(key)) {
+				SDS++;
+			}
+		}
+		System.out.println(SDS);
 	}
 }
