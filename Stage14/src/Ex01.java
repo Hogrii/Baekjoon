a20212df2ad953b16317a09bf3c4526d0a5406b3
jinho <hosboy93@naver.com>
2022-08-01 22:07:07 +0900

    BOJ 5086 배수와 약수 완료

--- Stage14/src/Ex01.java
+++ Stage14/src/Ex01.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Ex01 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
+		while(true) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			if(A==0&&B==0) {
+				System.exit(0);
+			}
+			
+			if(B%A==0) {
+				System.out.println("factor");
+			}else if(A%B==0) {
+				System.out.println("multiple");
+			}else {
+				System.out.println("neither");
+			}
+		}
 	}
 }
