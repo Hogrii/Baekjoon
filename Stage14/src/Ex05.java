3b2e196072ccf54a4663865ed7c95a9c7bc36937
jinho <hosboy93@naver.com>
2022-08-02 17:28:35 +0900

    BOJ 2981 검문 주석 추가

--- Stage14/src/Ex05.java
+++ Stage14/src/Ex05.java
@@ -7,21 +7,29 @@ public class Ex05 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		// 종이에 적은 숫자의 개수
 		int N = Integer.parseInt(br.readLine());
 		int paperInNum[] = new int[N];
 		
+		// 종이에 적은 숫자들
 		for(int i=0; i<N; i++) {
 			paperInNum[i] = Integer.parseInt(br.readLine());
 		}
 		
+		// 정렬
 		Arrays.sort(paperInNum);
 		
+		// 각 숫자들 차들의 공약수가 M이기 때문에 차를 구한다.
+		// 각 숫자들의 차가 음수가 되지 않게 정렬 후 빼주는 것
 		int gcfVal = paperInNum[1] - paperInNum[0];
 		
-		for(int i=2; i<N; i++) { // 6 34 38,, 28, 4 
+		// 최대공약수 찾기
+		for(int i=2; i<N; i++) {
+			// 바로 앞 최대공약수와 다음의 최대공약수 갱신
 			gcfVal = gcf(gcfVal, paperInNum[i] - paperInNum[i-1]);
 		}
 		
+		// 최대공약수의 약수 찾기 및 출력
 		for(int i=2; i<=gcfVal; i++) {
 			if(gcfVal%i == 0) {
 				System.out.print(i + " ");
@@ -29,6 +37,7 @@ public class Ex05 {
 		}
 	}
 	
+	// 최대공약수 메소드
 	public static int gcf(int a, int b) {		
 		while(b != 0) {
 			int r = a%b;
