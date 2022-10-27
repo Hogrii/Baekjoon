b4f67f1efa7b5c55851b50ecc7cfd32f6f334100
LeeJinHo <hosboy93@naver.com>
2022-10-27 10:09:26 +0900

    BOJ 1009 다리놓기 완료

--- Stage00/src/BOJ1009.java
+++ Stage00/src/BOJ1009.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 
 public class BOJ1009 {
 	public static void main(String[] args) {
-		//BOJ 1009 다리놓기
-		
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		int[] answer = new int[T];
+
+		for(int i = 0; i < T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int result = a;
+
+			if (a == 0) {
+				answer[i] = 1;
+				continue;
+			}
+
+			if (b == 1) {
+				result = a%10;
+			}else {
+				for(int j = 1; j < b; j++) {
+					result = result*a%10;
+				}
+			}
+
+			if (result == 0)
+				answer[i] = 10;
+			else
+				answer[i] = result;
+		}
+
+		for(int i = 0; i < T; i++) {
+			System.out.println(answer[i]);
+		}
 	}
 }
