18e8ee15c11a5a76ea35cf2a1c4ed15b528df3bf
LeeJinHo <hosboy93@naver.com>
2022-10-28 09:29:18 +0900

    BOJ 10807 개수 세기 완

---
+++ Stage04/src/BOJ10807.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10807 {
+	public static void main(String[] args) throws IOException{
+		// 개수 세기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		int cnt = 0;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int compare = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			if(arr[i] == compare) cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+	}
+}
