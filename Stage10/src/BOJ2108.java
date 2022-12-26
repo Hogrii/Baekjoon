87a7ba8a752ec0c99be68ebf8b10a68b7bc36b3b
jinho <hosboy93@naver.com>
2022-12-26 16:41:08 +0900

    BOJ2108 통계학
    단계별 문제풀기 단계 변경으로 인한 프로젝트 이동

--- Stage10/src/BOJ2108.java
+++ Stage10/src/BOJ2108.java
@@ -3,79 +3,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class BOJ2108 {
 	public static void main(String[] args) throws IOException{
 		// Åë°èÇÐ
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		ArrayList<Integer> modeArr = new ArrayList<>();
 		
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		int sum = 0;
+		int arr[] = new int[Integer.parseInt(br.readLine())];
 		
 		for(int i=0; i<arr.length; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
+		
+		br.close();
+
+		ArrayList<Integer> list = new ArrayList<>();
+		Arrays.sort(arr);
+		
+		int first = 0;
 		
 		// »ê¼úÆò±Õ
 		for(int i=0; i<arr.length; i++) {
-			sum += arr[i];
-		}
-		int avg = (int)Math.round((double)sum/N);
-		System.out.println(avg);
+			first += arr[i];
+			if(i==arr.length-1) {
+				first = Math.round(first/arr.length);
+				list.add(first);
+			}
+		}		
 		
 		// Áß¾Ó°ª
-		Arrays.sort(arr);
-		System.out.println(arr[N/2]);
+		list.add(arr[arr.length/2]);
 		
 		// ÃÖºó°ª
-		int cnt = 1;
-		int maxCnt = 0;
-		int maxVal = 1;
-		int secondVal = 1;
-		
-		if(N==1) System.out.println(arr[0]);
-		else {
-			for(int i=1; i<arr.length; i++) {
-				if(arr[i-1]==arr[i]) {
-					cnt++;
-				}else {
-					if(maxCnt<=cnt) {
-						maxVal = arr[i];
-						secondVal = maxVal;
-						maxCnt = cnt;
-						for(int j=0; j<maxCnt; j++) {
-							modeArr.add(arr[i]);							
-						}
-						cnt = 1;
-					}
-				}
-				
-				if(i==arr.length) {
-					if(maxCnt<=cnt) {
-						maxVal = arr[i];
-						secondVal = maxVal;
-						for(int j=0; j<maxCnt; j++) {
-							modeArr.add(arr[i]);							
-						}
-					}
-				}
-			}
-		}
-		if(secondVal==1) {
-			System.out.println(maxVal);
+		if(arr.length == 1) {
+			list.add(arr[0]);
 		}else {
-			int modeLength = modeArr.size();
-			System.out.println(modeArr[1]<>);
+			list.add(arr[1]);
 		}
 		
 		// ¹üÀ§
-		System.out.println(arr[N-1]-arr[0]);
-		
+		list.add(arr[arr.length-1]-arr[0]);
+
+		for(int result : list) {
+			sb.append(result).append('\n');
+		}
+		System.out.println(sb);
 	}
 }
